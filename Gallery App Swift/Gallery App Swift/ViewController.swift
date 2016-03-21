0362dff4efddc612cf890a2dba6f08083f89f036
Radhakrishna Pai <radhakrishna@qburst.com>
2016-03-21 10:11:44 +0530

    - initial project settings. - added git ignore file.

---
+++ Gallery App Swift/Gallery App Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery App Swift
+//
+//  Created by Pai on 21/03/16.
+//  Copyright © 2016 Pai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
